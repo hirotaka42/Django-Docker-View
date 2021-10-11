a445139f549042904e1cf9b01772f17011e1274d
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2021-10-11 13:46:34 +0900

    [update] Create a link to more information about Docker logs.

--- tail_docker_ps/urls.py
+++ tail_docker_ps/urls.py
@@ -7,4 +7,5 @@ tail_docker_ps > views.py からViewを参照
 
 urlpatterns = [
     path('', views.ps_list, name='ps_list'),
+    path('docker_logs/<str:container_id>/', views.logs_detail, name='logs_detail'),
 ]
--- tail_docker_ps/views.py
+++ tail_docker_ps/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from .models import Docker
+from django.shortcuts import render, get_object_or_404
 from .models import Docker
 
 # Create your views here.
@@ -7,3 +8,7 @@ def ps_list(request):
     dockerClass = Docker()
     docker_ps_list = dockerClass.ps_list()
     return render(request, 'tail_docker_ps/ps_list.html', {'docker_ps_list':docker_ps_list})
+
+def logs_detail(request, pk):
+    docker_id = get_object_or_404(Docker, container_id=container_id)
+    return render(request, 'tail_docker_ps/logs_view.html', {'docker_id': docker_id})
