7447a89a8536cf62fe18631e513a190116af31fc
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2021-10-16 20:33:07 +0900

    [update] STATUS display in log_view.html is now implemented.

--- tail_docker_ps/models.py
+++ tail_docker_ps/models.py
@@ -33,6 +33,30 @@ def is_Time_Calculation(seconds:int)->str:
         else:
             return str(is_Elapsed_time) +' seconds ago'
 
+def is_Time_Calculation_status(seconds:int)->str:
+    """
+    コンテナの経過時間を文字列で返却
+    """
+    is_Elapsed_time = seconds
+    is_Elapsed_minits_time = int(is_Elapsed_time / 60)
+    is_Elapsed_hours_time = int(is_Elapsed_minits_time / 60)
+    
+    if is_Elapsed_hours_time >= 1:
+        # 一時間以上経過している場合
+        if is_Elapsed_hours_time == 1:
+            return 'About an' +' hours'
+        else:
+            return str(is_Elapsed_hours_time ) +' hours'
+            
+    else:
+        # 一時間以内なら分数を返却
+        if is_Elapsed_minits_time == 1:
+            return 'About a' + ' minutes'
+        elif is_Elapsed_minits_time > 1:
+            return str(is_Elapsed_minits_time) +' minutes'
+        else:
+            return str(is_Elapsed_time) +' seconds'
+
 
 def ps_created(container) -> str:
     """
@@ -78,7 +102,7 @@ def ps_created(container) -> str:
             dt1 = datetime.datetime(int(is_created_dt[:4]), int(is_created_dt[5:7]), int(is_created_dt[8:10]), int(is_created_dt[11:13]), int(is_created_dt[14:16]), int(is_created_dt[17:19]))
             dt2 = datetime.datetime(int(is_dt_now[:4]), int(is_dt_now[5:7]), int(is_dt_now[8:10]), int(is_dt_now[11:13]), int(is_dt_now[14:16]), int(is_dt_now[17:19]))
             td1 = dt2 - dt1
-            if int(td1.days) >= 1:
+            if int(td1.days) > 0:
                 # 1日以上なら１日分の秒数を追加
                 is_Elapsed_time = int(td1.seconds) + 86400*int(td1.days)
                 CREATED = is_Time_Calculation(is_Elapsed_time)
@@ -88,6 +112,63 @@ def ps_created(container) -> str:
 
 
     return CREATED
+
+
+def ps_status(container) -> str:
+    """
+    引数のコンテナ情報からdocker ps 出力時の STATUSの値を返却する
+
+    """
+    days_tmp = []
+    str_y = ' years'
+    str_m = ' months'
+    str_w = ' weeks'
+    str_d = ' days'
+
+    if str(container.attrs['State']['StartedAt']) is None:
+        CREATED = 'None'
+    else:
+        is_created_dt = container.attrs['State']['StartedAt']
+        is_created_time = container.attrs['State']['StartedAt'][11:]
+        days_tmp = int(getPastDay(int(is_created_dt[:4]), int(is_created_dt[5:7]), int(is_created_dt[8:10])))
+        if days_tmp > 365:
+            #年表示
+            tmp_years = int(days_tmp / 360)
+            CREATED = str(tmp_years) + str_y
+        elif days_tmp > 30:
+            #月表示
+            tmp_moth = int(days_tmp / 30)
+            CREATED = str(days_tmp) + str_m
+        elif days_tmp > 7:
+            tmp_weeks = int(days_tmp / 7)
+            #週間表示
+            if tmp_weeks == 1:
+                CREATED = str(tmp_weeks) + ' week'
+            else:
+                CREATED = str(tmp_weeks) + str_w
+        elif days_tmp > 1:
+            #日にち表示
+            CREATED = str(days_tmp) + str_d
+        else:
+            # 2日以下なら hour, minutes, seconds,
+            #現在日時の取得 #docker コンテナのCreated値は'UTC'で定義されている
+            dt_now = datetime.datetime.now(timezone('UTC'))
+            #日時フォーマットをdocker ps createdに合わせる
+            is_dt_now = str(dt_now.isoformat())
+            dt1 = datetime.datetime(int(is_created_dt[:4]), int(is_created_dt[5:7]), int(is_created_dt[8:10]), int(is_created_dt[11:13]), int(is_created_dt[14:16]), int(is_created_dt[17:19]))
+            dt2 = datetime.datetime(int(is_dt_now[:4]), int(is_dt_now[5:7]), int(is_dt_now[8:10]), int(is_dt_now[11:13]), int(is_dt_now[14:16]), int(is_dt_now[17:19]))
+            td1 = dt2 - dt1
+            if int(td1.days) > 0:
+                # 1日以上なら１日分の秒数を追加
+                is_Elapsed_time = int(td1.seconds) + 86400*int(td1.days)
+                CREATED = is_Time_Calculation_status(is_Elapsed_time)
+            else:
+                is_Elapsed_time = int(td1.seconds)
+                CREATED = is_Time_Calculation_status(is_Elapsed_time)
+
+
+    return 'Up ' + CREATED
+
 
 def ps_port(container) -> str:
     """
@@ -178,7 +259,7 @@ class Docker():
             tmp['IMAGE'] = container.attrs['Config']['Image']
             tmp['COMMAND'] = container.attrs['Config']['Entrypoint'][0]
             tmp['CREATED'] = ps_created(container)
-            tmp['STATUS'] = 'STATUS'
+            tmp['STATUS'] = ps_status(container)
             tmp['PORT'] = ps_port(container)
             tmp['NAME'] = str(container.name)
             docker_ps.append(tmp)
